c3fd1c4066538076ed882a8783b6c5bf9dd41f70
apptunix <manpreetkumar@apptunix.com>
2021-08-27 17:53:27 +0530

    New Changes

--- src/app/_directive/directive.module.ts
+++ src/app/_directive/directive.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DefaultImage } from './defaultimg';
 import { MatInputCommifiedDirective } from './inputComma';
 
 @NgModule({
 	declarations: [
-		DefaultImage,
+		
 		MatInputCommifiedDirective
 	],
 	imports: [
 		CommonModule
-	], exports: [DefaultImage, MatInputCommifiedDirective]
+	], exports: [MatInputCommifiedDirective]
 })
 export class DirectiveModule { }
